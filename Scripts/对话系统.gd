a9bc33ab152d100dd55f4d6ec98031ec310537d6
闫辰祥 <1796655849@qq.com>
2025-04-18 10:12:19 +0800

    一些测试场景和对话系统

--- Scripts/对话系统.gd
+++ Scripts/对话系统.gd
@@ -1,56 +1,84 @@
 extends Control
 
+class Character:
+	var name: String
+	var tachie: Texture2D
+
+	func _init(nameParameter: String, tachieParameter: Texture2D = null) -> void:
+		self.name = nameParameter
+		self.tachie = tachieParameter
+
+
 # 自定义对话类
-class 对话:
-	# 对话内容
-	var 内容: String
-	# 角色名字
-	var 角色名字: String
+class DialogScript:
+	var content: String
+	var character: Character
 
 	# 构造函数
-	func _init(内容参数:String, 角色名字参数:String) -> void:
-		self.内容 = 内容参数
-		self.角色名字 = 角色名字参数
+	func _init(ContentParameter: String, CharacterParameter: Character = null) -> void:
+		self.content = ContentParameter
+		self.character = CharacterParameter
+	
 
-# 对话内容数组
-var 对话内容: Array = [
-	对话.new("你好，欢迎来到我们的游戏！", "旁白"),
-	对话.new("这是一个简单的对话系统示例。", "旁白"),
-	对话.new("每次点击都会显示下一句对话。", "旁白"),
-	对话.new("希望你喜欢这个示例！", "旁白")
-]
+
+
+
+
+var DialogScripts: Array = []
+
+
+
+
+	# 显示当前对话
+func ShowDialog() -> void:
+	$CanvasLayer.visible = true;
+	GameController.is_dialogue_active = true;
+	if nowIndex < DialogScripts.size():
+		var nowScript = DialogScripts[nowIndex]
+		ShowContent(nowScript.content)
+		if nowScript.character != null:
+			ShowCharacterName(nowScript.character)
+			ShowCharacterTachie(nowScript.character)
+	else:
+		textLabel.text = "对话结束。"
+		characterNameLabel.text = ""
+		关闭对话()
+		emit_signal("对话结束")
+
+
+func  ShowContent(content: String) -> void:
+	textLabel.text = content
 
 # 当前对话索引
-var 当前索引: int = 0
+var nowIndex: int = 0
 
 # 文本标签节点
-@export var 文本标签: Label
+@export var textLabel: Label
 
 # 角色名字标签节点
-@export var 角色名字标签: Label
+@export var characterNameLabel: Label
+
+# 角色立绘节点
+@export var characterTachieArea: TextureRect
 
 # 对话结束信号
 signal 对话结束
 
-# # Called when the node enters the scene tree for the first time.
-# func _ready() -> void:
-# 	# 显示初始对话
-# 	显示对话()
 
-# 显示当前对话
-func 显示对话() -> void:
-	$CanvasLayer.visible = true;
-	GameController.is_dialogue_active = true;
-	if 当前索引 < 对话内容.size():
-		var 当前对话 = 对话内容[当前索引]
-		文本标签.text = 当前对话.内容
-		角色名字标签.text = 当前对话.角色名字
+func ShowCharacterName(character: Character) -> void:
+	if character.name != "":
+		characterNameLabel.visible = true
+		characterNameLabel.text = character.name
 	else:
-		文本标签.text = "对话结束。"
-		角色名字标签.text = ""
-		# 发出对话结束信号
-		关闭对话()
-		emit_signal("对话结束")
+		characterNameLabel.visible = false
+
+
+func ShowCharacterTachie(character: Character) -> void:
+	if character.tachie != null:
+		print("角色名字：", character.name)
+		characterTachieArea.visible = true
+	else:
+		characterTachieArea.visible = false
 
 
 func 关闭对话() -> void:
@@ -59,11 +87,22 @@ func 关闭对话() -> void:
 	$CanvasLayer.visible = false;
 
 
-
-# 处理输入事件
 func _input(event: InputEvent) -> void:
 	# 检查是否是鼠标点击事件
 	if event is InputEventMouseButton and event.pressed:
 		# 增加当前索引以显示下一句对话
-		当前索引 += 1
-		显示对话()
+		nowIndex += 1
+		ShowDialog()
+
+func _ready() -> void:
+	var 映月 = Character.new("映月", load("res://Sprites/映月立绘-哭泣.png"))
+	var 旁白 = Character.new("旁白", null)
+	DialogScripts = [
+		DialogScript.new("第一句测试", 旁白),
+		DialogScript.new("角色测试", 映月),
+		DialogScript.new("无名字测试"),
+		DialogScript.new("然后出现名字测试", 映月)
+	]
+
+
+	ShowDialog()
